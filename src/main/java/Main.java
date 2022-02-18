0d2bfbf15e281155377c32e7936b140f655e9aa0
William Burdett <williamburdett1995@gmail.com>
2022-02-18 11:47:11 +0000

    added multiply method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world, this is person 1");
+        System.out.println(Calculator.multiply(2, 2));
     }
 }
